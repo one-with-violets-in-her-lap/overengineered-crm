f15fd05fab7ccf89e3b2b86b6b766bfd2850ff12
crucials <ensueness@gmail.com>
2024-10-11 12:46:38 +0500

    initialize two projects - next.js app and vite app

---
+++ clients-panel/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>
+        Clients
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ dashboard/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // transpilePackages: ['antd', '@ant-design'],
+    reactStrictMode: true,
+}
+
+export default nextConfig
