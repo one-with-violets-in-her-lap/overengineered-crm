64b320cb08461ddd46c9b478fe49ea669a36dda7
crucials <ensueness@gmail.com>
2024-10-11 13:04:44 +0500

    clients panel: add import alias and add table component

--- clients-panel/src/App.tsx
+++ clients-panel/src/App.tsx
@@ -1,9 +1,31 @@
+import ClientsTable from '@/components/ClientsTable'
+
 function App() {
   return (
     <>
       <h1>
         Clients
       </h1>
+
+      <ClientsTable clients={[
+        {
+          name: "client 1",
+          email: "client1@mail.com",
+          status: "in progress",
+        },
+
+        {
+          name: "client 2",
+          email: "client2@mail.com",
+          status: "completed",
+        },
+
+        {
+          name: "client 3",
+          email: "client3@mail.com",
+          status: "completed",
+        }
+      ]} />
     </>
   )
 }
---
+++ clients-panel/src/components/ClientsTable.tsx
@@ -0,0 +1,35 @@
+import { Client } from '@/types/client'
+
+export default function ClientsTable({ clients }: { clients: Client[] }) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+
+                    <th>Email</th>
+
+                    <th>Status</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {clients.map((client) => (
+                    <tr>
+                        <td>
+                            {client.name}
+                        </td>
+
+                        <td>
+                            {client.email}
+                        </td>
+                        
+                        <td>
+                            {client.status}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
